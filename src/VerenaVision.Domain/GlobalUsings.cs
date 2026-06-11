ff6000a17b741b6ed1ca26461c4a629bc3933f04
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-06-11 20:21:57 -0300

    Add VerenaVision.Domain project with initial configuration and project references

---
+++ src/VerenaVision.Domain/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using VerenaVision.Common.Attributes;
+global using VerenaVision.Common.Enums;
+global using VerenaVision.Common.Exceptions;
+global using VerenaVision.Common.Extensions;
+global using VerenaVision.Common.Factories;
+global using VerenaVision.Common.Helpers;
+global using VerenaVision.Shared.Abstractions;
---
+++ src/VerenaVision.Domain/Primitives/EntityBase.cs
@@ -0,0 +1,38 @@
+﻿namespace VerenaVision.Domain.Primitives;
+
+public abstract class EntityBase : IEntityBase
+{
+    public long Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime LastUpdatedAt { get; protected set; }
+
+    public override string ToString()
+    {
+        if (Id == 0)
+        {
+            return $"{GetType().Name}: Id=New";
+        }
+        return $"{GetType().Name}: Id={Id}, CreatedAt={CreatedAt}, LastUpdatedAt={LastUpdatedAt}";
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (ReferenceEquals(this, obj))
+            return true;
+
+        if (obj is null)
+            return false;
+
+        if (obj is not EntityBase other)
+            return false;
+
+        return obj.GetType() != GetType()
+            && Id > 0
+            && Id == other.Id;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(GetType(), Id);
+    }
+}
