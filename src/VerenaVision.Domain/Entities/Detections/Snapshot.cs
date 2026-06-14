376ec5ca176eec0c08154de76913e240c5ceaece
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-06-14 20:45:49 -0300

    Add Snapshot entity to represent detection snapshots
    Introduced a sealed Snapshot class in the Detections namespace. The class includes properties for detection reference, width, and height, with constructors for EF Core compatibility and input validation.

---
+++ src/VerenaVision.Domain/Entities/Detections/Snapshot.cs
@@ -0,0 +1,30 @@
+﻿namespace VerenaVision.Domain.Entities.Detections;
+
+public sealed class Snapshot : EntityBase
+{
+    public Guid Detection_Id { get; private set; }
+    public Detection Detection { get; private set; }
+    public int Width { get; private set; }
+    public int Height { get; private set; }
+
+    // EF Core constructor
+    private Snapshot()
+    {
+
+    }
+
+    internal Snapshot(
+        Detection detection,
+        int width, 
+        int height)
+    {
+        Guard.NotNull(detection);
+        Guard.Positive(width);
+        Guard.Positive(height);
+
+        Detection = detection;
+        Detection_Id = detection.Id;
+        Width = width;
+        Height = height;
+    }
+}
