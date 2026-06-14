b1a78daca7b76443b41f7f9023e43ab3369aa10d
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-06-14 20:45:17 -0300

    Add Device entity with tenant and event support
    Introduced the Device entity in the Automation namespace, implementing ITenantOwned, ISoftDeletableEntity, and IEventAggregate. Includes properties for tenant association, device details, soft deletion, and domain event management. Added EF Core private constructor.

---
+++ src/VerenaVision.Domain/Entities/Automation/Device.cs
@@ -0,0 +1,29 @@
+﻿using VerenaVision.Domain.Entities.Identities;
+
+namespace VerenaVision.Domain.Entities.Automation;
+
+public sealed class Device : EntityBase, ITenantOwned, ISoftDeletableEntity, IEventAggregate
+{
+    private readonly List<IDomainEvent> _events = [];
+    public Guid Tenant_Id { get; private set; }
+    public Tenant Tenant { get; private set; }
+    public string Name { get; private set; }
+    public string Description { get; private set; }
+    public string ControllerId { get; private set; }
+    public int PinNumber { get; private set; }
+    public DeviceStatus Status { get; private set; }
+
+    // EF Core constructor
+    private Device()
+    {
+    }
+     
+    #region ISoftDeletableEntity, IDomainEventAggregate
+    public bool IsDeleted { get; private set; }
+    public DateTime? DeletedAt { get; private set; }
+    public Guid? DeletedSession_Id { get; private set; }
+    IReadOnlyList<IDomainEvent> IEventAggregate.DomainEvents
+        => _events;
+   
+    #endregion
+}
