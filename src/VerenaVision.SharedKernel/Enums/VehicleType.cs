8b6ea0c88deb2e7f206391a0d1a2346374c782b6
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-06-14 17:31:53 -0300

    Add VehicleType enum to represent various vehicle types
    Introduced the VehicleType enum in the VerenaVision.Shared.Enums namespace, including values for Car, Truck, Bus, Motorcycle, Bicycle, Van, Trailer, Unknown, and an Undefined value with the [UndefinedValue] attribute. This provides a standardized way to represent vehicle types in the codebase.

---
+++ src/VerenaVision.SharedKernel/Enums/VehicleType.cs
@@ -0,0 +1,15 @@
+﻿namespace VerenaVision.Shared.Enums;
+
+public enum VehicleType
+{
+    [UndefinedValue]
+    Undefined = 0,
+    Car,
+    Truck,
+    Bus,
+    Motorcycle,
+    Bicycle,
+    Van,
+    Trailer,
+    Unknown
+}
