8b69387474e2e51227dda4222c56ec8a8a842d49
Rubens Cordeiro <rubens.cordeiro@live.com>
2026-06-14 20:45:39 -0300

    Add Detection entity with soft delete and event support
    Introduced the Detection entity in the Detections namespace, featuring properties for camera, frame, label, YOLO class, confidence, detection type, bounding box, and snapshots. Implements ISoftDeletableEntity and IEventAggregate for soft deletion and domain event handling. Includes constructors and validation for required fields.

---
+++ src/VerenaVision.Domain/Entities/Detections/Detection.cs
@@ -0,0 +1,57 @@
+﻿using VerenaVision.Domain.Entities.Cameras;
+
+namespace VerenaVision.Domain.Entities.Detections;
+
+public sealed class Detection : EntityBase, ISoftDeletableEntity, IEventAggregate
+{
+    private readonly List<IDomainEvent> _events = [];
+    private readonly List<Snapshot> _snapshots= [];
+    public int? TrackingId { get; private set; }
+    public Guid Camera_Id { get; private set; }
+    public Camera Camera { get; private set; }
+    public long FrameNumber { get; private set; }
+    public string Label { get; init; }
+    public YoloClass YoloClass { get; init; }
+    public double Confidence { get; private set; }
+    public DetectionType DetectionType { get; set; }
+    public BoundingBox BoundingBox { get; private set; }
+
+    public IReadOnlyList<Snapshot> Snapshots
+        => _snapshots;
+    private Detection()
+    {
+    }
+
+    internal Detection(
+        Camera camera,
+        long frameNumber,
+        YoloClass yoloClass,
+        string label,
+        double confidence,
+        DetectionType detectionType,
+        BoundingBox boundingBox,
+        int? trackingId = null)
+    {
+        Guard.NotNull(camera);
+        Guard.NotNullOrWhiteSpace(label);
+        Guard.NotNull(boundingBox);
+        
+        Camera = camera;
+        Camera_Id = camera.Id;
+        FrameNumber = frameNumber;
+        YoloClass = yoloClass;
+        Label = label;
+        Confidence = confidence;
+        DetectionType = detectionType;
+        BoundingBox = boundingBox;
+        TrackingId = trackingId;
+    }
+
+    #region ISoftDeletableEntity, IDomainEventAggregate
+    public bool IsDeleted { get; private set; }
+    public DateTime? DeletedAt { get; private set; }
+    public Guid? DeletedSession_Id { get; private set; }
+    IReadOnlyList<IDomainEvent> IEventAggregate.DomainEvents
+        => _events;
+    #endregion
+}
